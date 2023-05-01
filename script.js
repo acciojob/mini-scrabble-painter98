381515ef2009e9738bf1d19fccf7e1b5c8073d2c
painter98 <120320713+painter98@users.noreply.github.com>
2023-05-01 18:56:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 //your code here
 let text = document.getElementById('evaluatedText');
 let letters = document.getElementById('letterCount');
-let count=0;
 
 text.addEventListener('input',()=>{
-	count++;
-	letters.innerText = count;
+	let str = text.value;
+	letters.innerText = str.length;
 });
